3b6072db4b487015a048460aed5c482ba1dea79c
Giulia <giuliapupo0@gmail.com>
2020-11-23 17:21:07 +0100

    Aggiungere i file di progetto.

---
+++ prgUnicode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace prgUnicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String unicodeString =
+                "This Unicode string has 2 characters outside the" +
+                "ASCII range: \n" +
+                "Pi (\u03A0), and Sigma (\u03A3).";
+            Console.WriteLine("Original string:");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodeString);
+            Console.WriteLine();
+        }
+    }
+}
