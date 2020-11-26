6bcf9d14786d7a3d81be2bae52390c83560f697f
Giulia <giuliapupo0@gmail.com>
2020-11-26 12:12:45 +0100

    unicode

--- prgUnicode/Program.cs
+++ prgUnicode/Program.cs
@@ -6,18 +6,18 @@ namespace prgUnicode
     {
         static void Main(string[] args)
         {
-            String unicodeString =
-                "This Unicode string has 2 characters outside the" +
-                "ASCII range: \n" +
-                "Pi (\u03A0), and Sigma (\u03A3).";
+            //String unicodeString =
+            //    "This Unicode string has 2 characters outside the" +
+            //    "ASCII range: \n" +
+            //    "Pi (\u03A0), and Sigma (\u03A3).";
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            string unicodeString = "";
+            Console.WriteLine("Inserisci una frase:");
+            unicodeString = Console.ReadLine();
             Console.WriteLine("Original string:");
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
             Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine("Inserisci una frase:");
-            //Console.ReadLine();
-            //quest'ultimo comando è necessario per far scrivere a console una frase.
         }
     }
 }
